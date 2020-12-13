53171d7770cb585e69f1db2e5cd697c3d3e8649a
Monika <m.szymanska.macik@gmail.com>
2020-12-13 15:06:13 +0100

    projecting data

--- SamuraiApp/ConsoleApp/Program.cs
+++ SamuraiApp/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp
@@ -12,14 +13,22 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            AddSamurai();
-            GetSamurais("After Add:");
-            InsertMultipleSamurais();
-            QueryFilters();
-            RetrieveAndUpdateSamurai();
-            RetrieveAndUpdateMultipleSamurais();
-            MultipleDatabaseOperations();
-            RetrieveAndDeleteASamurai();
+            //AddSamurai();
+            //GetSamurais("After Add:");
+            //InsertMultipleSamurais();
+            //QueryFilters();
+            //RetrieveAndUpdateSamurai();
+            //RetrieveAndUpdateMultipleSamurais();
+            //MultipleDatabaseOperations();
+            //InsertNewSamuraiWithAQuote();
+            //InsertNewSamuraiWithMayQuotes();
+            //AddQuoteToExistingSamuraiWhileTracked();
+            ////RetrieveAndDeleteASamurai();
+            //AddQuoteToExistingSamuraiNotTracked(2);
+            //AddQuoteToExistingSamuraiNotTracked_Easy(2);
+            //EagerLoadSamuraiWithQuotes();
+            //ProjectSomeProperties();
+            //ProjectSamuraiWithHappyQuotes();
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
@@ -77,11 +86,111 @@ namespace ConsoleApp
             _context.Samurais.Add(new Samurai { Name = "Kikuchiypo" });
             _context.SaveChanges();
         }
-        private static void RetrieveAndDeleteASamurai()
+        ////private static void RetrieveAndDeleteASamurai()
+        //{
+        //    var samurai = _context.Samurais.Find(18);
+        //    _context.Samurais.Remove(samurai);
+        //    _context.SaveChanges();
+        //}
+        private static void InsertBattle()
         {
-            var samurai = _context.Samurais.Find(18);
-            _context.Samurais.Remove(samurai);
+            _context.Battles.Add(new Battle
+            {
+                Name = "Battle of Okehazma",
+                StartDate = new DateTime(1560, 05, 01),
+                EndDate = new DateTime(1560, 06, 15)
+            });
             _context.SaveChanges();
+        }
+        private static void QueryAndUpdateBattle_Disconnected()
+        {
+            var battle = _context.Battles.AsNoTracking().FirstOrDefault();
+            battle.EndDate = new DateTime(1560, 06, 30);
+            using(var newContextInstace = new SamuraiContext())
+            {
+                newContextInstace.Battles.Update(battle);
+                newContextInstace.SaveChanges();
+            }
+        }
+        private static void InsertNewSamuraiWithAQuote()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kambei Shimaba",
+                Quotes = new List<Quote>
+                {
+                    new Quote {Text = "I've come to save yu"}
+                }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();       
+        }
+        private static void InsertNewSamuraiWithMayQuotes()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kambei",
+                Quotes = new List<Quote>
+                {
+                    new Quote {Text = "Watch out for my sharp sword!"},
+                    new Quote {Text = "I've come to save yu"}
+                }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+        private static void AddQuoteToExistingSamuraiWhileTracked()
+        {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Quotes.Add(new Quote
+            {
+                Text = "I bet you're happy that I've saved you!"
+            });
+            _context.SaveChanges();
+        }
+        private static void AddQuoteToExistingSamuraiNotTracked(int samuraiId)
+        {
+            var samurai = _context.Samurais.Find(samuraiId);
+            samurai.Quotes.Add(new Quote
+            {
+                Text = "Now that I saved you, you will feed me deinner?"
+            });
+            using(var newContext = new SamuraiContext())
+            {
+                newContext.Samurais.Update(samurai);
+                newContext.SaveChanges();
+            }
+        } 
+        private static void AddQuoteToExistingSamuraiNotTracked_Easy(int samuraiId)
+        {
+            var quote = new Quote
+            {
+                Text = "Now that I saved you, will you feed me dinner again?",
+                SamuraiId = samuraiId
+            };
+            using(var newContext = new SamuraiContext())
+            {
+                newContext.Quotes.Add(quote);
+                newContext.SaveChanges();
+            }
+        }
+        private static void EagerLoadSamuraiWithQuotes()
+        {
+            var samuraiWithQuotes = _context.Samurais.Include(s => s.Quotes).ToList();
+        }
+        private static void ProjectSomeProperties()
+        {
+            var someProperties = _context.Samurais.Select(s => new { s.Id, s.Name }).ToList();
+        }
+        private static void ProjectSamuraiWithHappyQuotes()
+        {
+            var samuraiWithHappyQuote = _context.Samurais.Select(s => new { Samurais = s, HappyQuotes = s.Quotes.Where(q => q.Text.Contains("happy")) }).ToList();
+           }
+        private static void FilteringWithRelatedData()
+        {
+            var samurais = _context.Samurais
+                .Where(s => s.Quotes.Any(q => q.Text.Contains("happy")))
+                .ToList();
         }
     }
 }
--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -10,6 +10,7 @@ namespace SamuraiApp.Data
     public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
 
